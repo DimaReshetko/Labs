e8636af886df6f8c4918bc287bc58b31575033f5
DimaReshetko <lbvflfqvjy@gmail.com>
2024-11-26 21:56:53 +0700

    реализован алгоритм Шелла для сортировки массива. Также, для проверки работы алгоритма написал функцию проверки отсортированного массива на упорядоченность.

---
+++ ASD_3/ASD_3/ASD_3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <locale>
+
+void shell(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int d = n / 2; d > 0; d /= 2) {
+        for (int i = d; i < n; i++) {
+            int t = arr[i];
+            int j;
+            for (j = i; j >= d && arr[j - d] > t; j -= d) {
+                arr[j] = arr[j - d];
+            }
+            arr[j] = t;
+        }
+    }
+}
+
+bool proverka(const std::vector<int>& arr) {
+    for (size_t i = 1; i < arr.size(); i++ ) {
+        if (arr[i - 1] > arr[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    std::vector<int> arr = { 12, 34, 54, 2, 3, 11, 7, 89, 45, 67 };
+
+    std::cout << "Исходный массив: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    shell(arr);
+
+    std::cout << "Отсортированный массив: ";
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    if (proverka(arr)) {
+        std::cout << "Массив отсортирован" << std::endl;
+    }
+    else {
+        std::cout << "Массив не отсортирован" << std::endl;
+    } 
+
+    return 0;
+}
